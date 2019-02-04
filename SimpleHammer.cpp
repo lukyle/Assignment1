73db94d56bca60fc3ca5464cdb6824338304f8da
Kyle Lu <kylejinglu99@gmail.com>
2019-02-03 20:54:03 -0500

    Added SimpleHammer files (new weapon)
    Added SimpleHammer weapons files with respective hit damage

---
+++ SimpleHammer.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File: SimpleHammer.cpp
+ * Author: Kyle Lu kjl17@my.fsu.edu
+ * 
+ * Created on February 3rd, 2019
+ */
+
+#include "SimpleHammer.h"
+
+double SimpleHammer::hit(double armor) {
+    double damage;
+    if(armor < 30){
+    	damage = hitPoints - (armor * 0); // armor is totally ignored
+    }
+
+    if (damage < 0) {
+        return 0;
+    }
+    return damage;
+}
